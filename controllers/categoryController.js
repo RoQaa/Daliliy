f653bde325ffb30629fed6676042c7dfe6ab3fe7
Farouk Adel <farouk.adel931@gmail.com>
2024-04-09 21:11:04 +0200

    return to jpeg

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,11 +29,11 @@ const multerFilter = (req, file, cb) => {
   exports.resizeCatPhoto = catchAsync(async (req, res, next) => {
     if (!req.file) return next();
 
-    req.file.filename = `cat-${req.params.id}-${Date.now()}.jpg`;
+    req.file.filename = `cat-${req.params.id}-${Date.now()}.jpeg`;
   
     await sharp(req.file.buffer)
       .resize(500, 500)
-      .toFormat('jpg')
+      .toFormat('jpeg')
       .jpeg({ quality: 90 })
       .toFile(`public/img/cats/${req.file.filename}`);
   
